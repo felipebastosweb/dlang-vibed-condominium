18a32dca73c42a13248aef5e9c6afb14e2e7c186
felipe bastos <felipebastosweb@gmail.com>
2019-09-18 18:44:34 -0300

    Update models.py

--- planning/models.py
+++ planning/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Resource(models.Model):
+    name = models.CharField()
+    type = models.CharField(max_length=255)
+    cost = models.DecimalField(19, 2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    start_at = models.DateField()
+    ends_at = models.DateField()
+    predecessor = models.ForeignKey(Task, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
