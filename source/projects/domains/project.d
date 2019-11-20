462b0ebbdb34edaab3e7743de8f0e41e4841ccbd
felipe bastos <felipebastosweb@gmail.com>
2019-11-20 09:57:00 -0300

    Create project.d

---
+++ source/projects/domains/project.d
@@ -0,0 +1,9 @@
+module projects.domains.project;
+
++++
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
++++
